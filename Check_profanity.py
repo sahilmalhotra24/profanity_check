73e64eaf7e27df54354b7e2a2d9f26b5f9bffc27
Sahil Malhotra <sahilmalhotra578@gmail.com>
2017-08-22 18:28:23 +0530

    Create Check_profanity.py

---
+++ Check_profanity.py
@@ -0,0 +1,21 @@
+print("************* What Do you love (Implementation) *****************")
+import urllib
+def read_text():
+	quotes = open("movie_quotes.txt")
+	contents = quotes.read()
+	print(contents)
+	quotes.close()
+	check_profanity(contents)
+
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+	output = connection.read()
+	#print(output) # false->no curse word, if True -> curse word present
+	connection.close()
+	if "true" in output:
+		print("Profanity Alert!!")
+	elif "false" in output:
+		print("This Documents has No Curse Words")
+	else:
+		print("Couldn't scan document")
+read_text()
