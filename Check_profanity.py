8faeb605cacbf6f4ebcad985e527728f44b36132
Sahil Malhotra <sahilmalhotra578@gmail.com>
2017-09-23 08:59:21 +0530

    Update Check_profanity.py

--- Check_profanity.py
+++ Check_profanity.py
@@ -6,7 +6,7 @@ def read_text():
 	print(contents)
 	quotes.close()
 	check_profanity(contents)
-##
+#
 def check_profanity(text_to_check):
 	connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
 	output = connection.read()
